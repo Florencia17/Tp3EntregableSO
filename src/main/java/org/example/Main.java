26bca56d867c4ad22547b97ff8959f025befd35e
florencia17 <flormalacarne@gmail.com>
2023-11-12 20:53:26 -0300

    tp terminado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Aerosilla silla = new Aerosilla(4, 1); // 8 esquiadores, 2 sillas cuádruples
+        Aerosilla silla = new Aerosilla(4, 1); // 4 esquiadores, 1 silla cuádruples
         silla.iniciarSimulacion();
     }
 }
