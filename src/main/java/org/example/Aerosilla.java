b03cbed712d80439d5d27ea3cda894f5636294ff
florencia17 <flormalacarne@gmail.com>
2023-11-12 19:29:36 -0300

    tp terminado

--- src/main/java/org/example/Aerosilla.java
+++ src/main/java/org/example/Aerosilla.java
@@ -4,15 +4,15 @@ import java.util.concurrent.Semaphore;
 
 public class Aerosilla {
     private final int N; // Número de esquiadores
-    private final int M; // Número de sillas cuádruples
-    private Semaphore sillasDisponibles; // Semáforo para gestionar las sillas disponibles
-    private Semaphore mutex; // Semáforo para garantizar la exclusión mutua
+    private final int M; // Número de sillas
+    private Semaphore sillasDisponibles; // Semáforo para las sillas disponibles
+    private Semaphore mutex; // Semáforo para la exclusión mutua
 
     public Aerosilla(int N, int M) {
         this.N = N;
         this.M = M;
-        this.sillasDisponibles = new Semaphore(M * 4); // Inicializamos con capacidad para M sillas cuádruples
-        this.mutex = new Semaphore(1); // Inicializamos el semáforo de exclusión mutua
+        this.sillasDisponibles = new Semaphore(M * 4); // capacidad para M sillas que son cuadruples
+        this.mutex = new Semaphore(1);
     }
 
     public void iniciarSimulacion() {
@@ -23,16 +23,16 @@ public class Aerosilla {
     }
 
     public void tomarSilla() throws InterruptedException {
-        sillasDisponibles.acquire(); // Espera si no hay sillas disponibles
-        mutex.acquire(); // Entramos en la sección crítica
+        sillasDisponibles.acquire();
+        mutex.acquire();
         System.out.println("Esquiador " + Thread.currentThread().getId() + " toma una silla.");
-        mutex.release(); // Salimos de la sección crítica
+        mutex.release();
     }
 
     public void liberarSilla() throws InterruptedException {
-        mutex.acquire(); // Entramos en la sección crítica
+        mutex.acquire();
         System.out.println("Esquiador " + Thread.currentThread().getId() + " libera la silla.");
-        mutex.release(); // Salimos de la sección crítica
-        sillasDisponibles.release(); // Liberamos una silla
+        mutex.release();
+        sillasDisponibles.release();
     }
 }
--- src/main/java/org/example/Esquiador.java
+++ src/main/java/org/example/Esquiador.java
@@ -16,9 +16,9 @@ public class Esquiador extends Thread {
         while (true) {
             try {
                 silla.tomarSilla(); // El esquiador toma una silla
-                Thread.sleep(1000); // Simula el tiempo que tarda en llegar a la cima
+                Thread.sleep(1000);
                 silla.liberarSilla(); // El esquiador libera la silla en la cima
-                Thread.sleep(2000); // Simula el tiempo que tarda en esquiar de vuelta
+                Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
